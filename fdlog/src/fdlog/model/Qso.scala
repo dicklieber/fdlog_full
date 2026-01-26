4744888ff0913d044d9eca6166269a5fc9060224
dlieber <dick@u505.com>
2026-01-25 18:01:04 -0600

    ckpoint

---
+++ fdlog/src/fdlog/model/Qso.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2026. Dick Lieber, WA9NNN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package fdlog.model
+
+
+import fdlog.util.GenerateId
+
+import java.time.Instant
+import java.util.UUID
+
+
+/**
+ * This is what's in the store and journal.log.
+ *
+ * @param callSign    of the worked station.
+ * @param exchange    from the worked station.
+ * @param bandMode    that was used.
+ * @param mHz         may not be known if not connectd to a rig
+ * @param stamp       when QSO occurred.
+ * @param uuid        id unique QSO id in time & space.
+ * @param qsoMetadata info about ur station.
+ */
+case class Qso(callSign: CallSign,
+               exchange: Exchange,
+               bandMode: BandMode,
+               qsoMetadata: QsoMetadata,
+               mHz: Option[Float] = None,
+               stamp: Instant = Instant.now(),
+               uuid: String = GenerateId.generate()) extends Ordered[Qso]:
+  def isDup(that: Qso): Boolean =
+    this.callSign == that.callSign &&
+      this.bandMode == that.bandMode
+
+
+type CallSign = String
+type Band = String
+type Mode = String
+
+
+
+
