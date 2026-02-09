7eb2293fc0399179db620c36da929882f8e6f6fd
dlieber <dick@u505.com>
2026-02-09 12:24:16 -0600

    1st hookup of DupDetector.
    Signed-off-by: dlieber <dick@u505.com>

--- fdswarm/src/fdswarm/fx/CallSignField.scala
+++ fdswarm/src/fdswarm/fx/CallSignField.scala
@@ -23,7 +23,8 @@ import _root_.scalafx.Includes.*
 import _root_.scalafx.scene.control.TextField
 import _root_.scalafx.scene.input.{KeyCode, KeyEvent}
 import fdswarm.fx.CallSignField.regex
-import fdswarm.model.DupQsoDetector
+import fdswarm.fx.bandmodes.{BandModeStore, SelectedBandModeStore}
+import fdswarm.store.QsoStore
 import jakarta.inject.Inject
 
 import scala.util.matching.Regex
@@ -33,16 +34,22 @@ import scala.util.matching.Regex
  * sad or happy as validated while typing.
  *
  */
-class CallSignField @Inject()(dupQsoDetector: DupQsoDetector) extends TextField with NextField :
+class CallSignField @Inject()(qsoStore: QsoStore, selectedBsndModeStore: SelectedBandModeStore) extends TextField with NextField:
   styleClass += "qsoCallSign"
 
   //  def onNextField(ch:Char => Unit):Unit=
   //    onKeyReleased = event => ch(event.getText.head)
 
 
-  def isValid(str: String): Boolean = CallSignField.isValid(str)
+  def isValid(str: String): Boolean =
+    val mayDups = qsoStore.potentialDups(str, selectedBsndModeStore.selected.value)
+    logger.info(s"Potential duplicates: $mayDups")
+    CallSignField.isValid(str)
 
 object CallSignField:
-  def isValid(str: String): Boolean = regex.findFirstIn(str).isDefined
+
   protected val regex: Regex = """^(?=.{3,12}$)[A-Z0-9]{1,3}[0-9][A-Z0-9]{1,4}(?:/(?:P|M|MM|AM|QRP|[A-Z0-9]{1,4}))?$""".r
+
+  def isValid(str: String): Boolean =
+    regex.findFirstIn(str).isDefined
 
