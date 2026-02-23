1e455d67ecd500776f16b962a3f89db8a0e94405
dlieber <dick@u505.com>
2026-02-23 12:17:45 -0600

    add buildAndRun.sh
    Signed-off-by: dlieber <dick@u505.com>

---
+++ build-jars.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026. Dick Lieber, WA9NNN
+#
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or    
+# (at your option) any later version.                                  
+#                                                                      
+# This program is distributed in the hope that it will be useful,      
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+# GNU General Public License for more details.                         
+#                                                                      
+# You should have received a copy of the GNU General Public License    
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+set -euo pipefail
+
+echo "Building fdswarm and fdswarmClient JARs..."
+./mill fdswarm.assembly
+./mill fdswarmClient.assembly
+
+echo "Copying JARs to current directory..."
+cp -v "out/fdswarm/assembly.dest/out.jar" "fdswarm.jar"
+cp -v "out/fdswarmClient/assembly.dest/out.jar" "fdswarmClient.jar"
+
+echo "Build and copy complete:"
+ls -lh fdswarm.jar fdswarmClient.jar
---
+++ buildAndRun.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2026. Dick Lieber, WA9NNN
+#
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or    
+# (at your option) any later version.                                  
+#                                                                      
+# This program is distributed in the hope that it will be useful,      
+# but WITHOUT ANY WARRANTY; without even the implied warranty of       
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+# GNU General Public License for more details.                         
+#                                                                      
+# You should have received a copy of the GNU General Public License    
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+set -euo pipefail
+
+# Prompt for howManyNodes (Servers), default to 2
+read -p "How many server nodes? [2]: " howManyNodes
+howManyNodes=${howManyNodes:-2}
+
+# Prompt for howManyClients, default to 1
+read -p "How many client nodes? [1]: " howManyClients
+howManyClients=${howManyClients:-1}
+
+echo "Building JARs..."
+./build-jars.sh
+
+echo "Starting swarm using java -jar..."
+
+pids=()
+pgids=()
+
+# Run each node (Server)
+for (( i=0; i<howManyNodes; i++ ))
+do
+    PORT=$((8080 + i))
+    echo "Starting server node $i on port $PORT..."
+    # Ensure logs directory exists
+    LOG_DIR="$HOME/fdswarm/$PORT"
+    mkdir -p "$LOG_DIR"
+    
+    # Run java -jar in background
+    # Start in a new process group so we can kill everything later
+    # We pass PORT as an environment variable as the app seems to expect it
+    PORT=$PORT java -jar fdswarm.jar > "$LOG_DIR/stdout.log" 2>&1 &
+    pids+=($!)
+    pgids+=($(ps -o pgid= -p $!))
+    sleep 2
+done
+
+# Run each client
+for (( i=0; i<howManyClients; i++ ))
+do
+    # Map clients to servers in a round-robin fashion
+    SERVER_PORT=$((8080 + (i % howManyNodes)))
+    CLIENT_PORT=$((9090 + i))
+    echo "Starting client $i connecting to server on port $SERVER_PORT (client local dir port suffix $CLIENT_PORT)..."
+    
+    LOG_DIR="$HOME/fdswarm/client-$CLIENT_PORT"
+    mkdir -p "$LOG_DIR"
+
+    # We use PORT here for ClientDirectoryProvider to distinguish local config/logs
+    # And SERVER_PORT for FdSwarmRestClient to know where to connect
+    PORT=$CLIENT_PORT SERVER_PORT=$SERVER_PORT java -jar fdswarmClient.jar > "$LOG_DIR/stdout.log" 2>&1 &
+    pids+=($!)
+    pgids+=($(ps -o pgid= -p $!))
+    sleep 2
+done
+
+echo "All $howManyNodes servers and $howManyClients clients started."
+# Use /dev/tty for read to ensure it works even if stdin is redirected
+read -p "Press any key to stop all nodes..." -n1 -s < /dev/tty
+echo ""
+
+# Terminate all nodes and their children
+for pgid in "${pgids[@]}"
+do
+    # Trim whitespace
+    pgid=$(echo $pgid | tr -d ' ')
+    if [ -n "$pgid" ]; then
+        echo "Stopping node process group with PGID $pgid..."
+        # Kill the entire process group
+        kill -TERM -"$pgid" 2>/dev/null
+    fi
+done
+
+# Wait for all background processes to finish
+wait
+echo "All nodes stopped."
