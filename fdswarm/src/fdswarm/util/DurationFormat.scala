d16e0c50c16548a396b9346d735748d0c133e6a4
dlieber <dick@u505.com>
2026-03-10 13:44:54 -0500

    Fix warning, clean up.
    Signed-off-by: dlieber <dick@u505.com>

--- fdswarm/src/fdswarm/util/DurationFormat.scala
+++ fdswarm/src/fdswarm/util/DurationFormat.scala
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2026. Dick Lieber, WA9NNN
  *
- * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or    
- * (at your option) any later version.                                  
- *                                                                      
- * This program is distributed in the hope that it will be useful,      
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
- * GNU General Public License for more details.                         
- *                                                                      
- * You should have received a copy of the GNU General Public License    
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
@@ -39,25 +39,26 @@ package fdswarm.util
 import java.time.{Duration, Instant}
 import scala.language.implicitConversions
 
-/**
- * Format a [[java.time.Duration]] to nice human readable, scaled string.
- */
-object DurationFormat {
+/** Format a [[java.time.Duration]] to nice human readable, scaled string.
+  */
+object DurationFormat:
 
-  val ONE_SECOND = 1000L
-  val ONE_MINUTE = 60000L
-  val ONE_HOUR = 3600000L
-  val ONE_DAY = 86400000L
+  private val ONE_SECOND = 1000L
+  private val ONE_MINUTE = 60000L
+  private val ONE_HOUR = 3600000L
+  private val ONE_DAY = 86400000L
 
+  def apply(instant: Instant): String =
+    apply(Duration.between(instant, Instant.now()))
 
-  /**
-   *
-   * @param duration how long
-   * @return string e.g. "59 min 2 sec" or "999 ms"
-   */
-  implicit def apply(duration: Duration): String = {
-    duration.toMillis match {
-      case ms if ms < ONE_SECOND => f"${duration.toMillis} ms"
+  /** @param duration
+    *   how long
+    * @return
+    *   string e.g. "59 min 2 sec" or "999 ms"
+    */
+  implicit def apply(duration: Duration): String =
+    duration.toMillis match
+      case ms if ms < ONE_SECOND  => f"${duration.toMillis} ms"
       case ms if ms == ONE_SECOND => "1 sec"
       case ms if ms < ONE_MINUTE =>
         val remaininMs = ms % ONE_SECOND
@@ -75,16 +76,5 @@ object DurationFormat {
         val days = ms / ONE_DAY
         val hours = (ms - days * ONE_DAY) / ONE_HOUR
         val mins = (ms - (hours * ONE_HOUR + days * ONE_DAY)) / ONE_MINUTE
-        if (mins == 0) {
-          f"$days%d day $hours%d hour"
-        } else {
-          f"$days%d day $hours%d hour $mins%d min"
-        }
-    }
-  }
-
-  def apply(instant: Instant): String = {
-    apply(Duration.between(instant, Instant.now()))
-  }
-
-}
+        if mins == 0 then f"$days%d day $hours%d hour"
+        else f"$days%d day $hours%d hour $mins%d min"
