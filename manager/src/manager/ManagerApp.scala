427d4e1f5782a637c0dd21e753bf7afca65e86aa
dlieber <dick@u505.com>
2026-03-18 12:55:22 -0500

    manager uses grid instread of table.
    Signed-off-by: dlieber <dick@u505.com>

--- manager/src/manager/ManagerApp.scala
+++ manager/src/manager/ManagerApp.scala
@@ -21,23 +21,22 @@ package manager
 import com.google.inject.{Guice, Injector}
 import com.typesafe.scalalogging.LazyLogging
 import fdswarm.DebugConfig
-import fdswarm.fx.bandmodes.{BandModeMatrixPane, SelectedBandModeStore}
 import fdswarm.fx.bands.*
 import fdswarm.model.{BandMode, Callsign}
 import fdswarm.util.CallsignGenerator
-import scala.util.Random
 import net.codingwell.scalaguice.InjectorExtensions.*
-import scalafx.Includes.*
 import scalafx.application.JFXApp3
-import scalafx.beans.property.{BooleanProperty, ObjectProperty, StringProperty}
 import scalafx.scene.Scene
-import scalafx.scene.control.*
-import scalafx.scene.control.TableColumn.*
-import scalafx.scene.control.cell.{CheckBoxTableCell, TextFieldTableCell}
+import scalafx.scene.control.Button
 import scalafx.scene.layout.{BorderPane, HBox}
 
 import scala.collection.IndexedSeqView
+import scala.util.Random
 
+/**
+ * A development tool tha runs a bunch of instances of the FDSwarm application
+ * on this host.
+ */
 object ManagerApp extends JFXApp3 with LazyLogging :
 
   private lazy val injector: Injector =
@@ -69,141 +68,11 @@ object ManagerApp extends JFXApp3 with LazyLogging :
       //      }
       scene = new Scene {
         root = new BorderPane {
-          center = new TableView[DebugConfig](nodeConfigManager.observableBuffer) {
-            editable = true
-            columns ++= Seq(
-              new TableColumn[DebugConfig, String] {
-                text = "Id"
-                cellValueFactory = { cd => new StringProperty(cd.value, "id", cd.value.id) }
-                editable = false
-              },
-              new TableColumn[DebugConfig, String] {
-                text = "Operator"
-                cellValueFactory = { cd => new StringProperty(cd.value, "operator", cd.value.operator.value) }
-                cellFactory = (col: TableColumn[DebugConfig, String]) => {
-                  new TextFieldTableCell[DebugConfig, String](new scalafx.util.StringConverter[String] {
-                    override def toString(t: String): String = t
-
-                    override def fromString(s: String): String = s
-                  }) {
-                    graphic.onChange { (_, _, newValue) =>
-                      if (newValue != null && newValue.isInstanceOf[javafx.scene.control.TextField]) {
-                        val textField = newValue.asInstanceOf[javafx.scene.control.TextField]
-                        textField.setTextFormatter(new javafx.scene.control.TextFormatter[String](new java.util.function.UnaryOperator[javafx.scene.control.TextFormatter.Change] {
-                          override def apply(change: javafx.scene.control.TextFormatter.Change): javafx.scene.control.TextFormatter.Change = {
-
-                            if (change.isContentChange) {
-                              change.setText(change.getText.toUpperCase)
-                            }
-                            change
-                          }
-                        }))
-                      }
-                    }
-                  }
-                }
-                onEditCommit = (evt: TableColumn.CellEditEvent[DebugConfig, String]) => {
-                  val index = evt.tablePosition.row
-                  val old = nodeConfigManager.observableBuffer(index)
-                  nodeConfigManager.observableBuffer(index) = old.copy(operator = Callsign(evt.newValue))
-                }
-                editable = true
-              },
-              new TableColumn[DebugConfig, String] {
-                text = "BandMode"
-                cellValueFactory = { cd => new StringProperty(cd.value, "bandMode", cd.value.bandMode.toString) }
-                cellFactory = (col: TableColumn[DebugConfig, String]) => {
-                  new TableCell[DebugConfig, String] {
-                    item.onChange { (_, _, newValue) =>
-                      text = newValue
-                    }
-                    onMouseClicked = _ => {
-                      val index = tableRow.value.indexProperty().get()
-                      if (index >= 0 && index < nodeConfigManager.observableBuffer.size) {
-                        val oldConfig = nodeConfigManager.observableBuffer(index)
-
-                        // We need a BandModeMatrixPane. Since it's a singleton in this injector,
-                        // we can just get it. But we must set its initial selection.
-                        val matrixPane = injector.instance[BandModeMatrixPane]
-                        val selectedStore = injector.instance[SelectedBandModeStore]
-                        selectedStore.save(oldConfig.bandMode)
-
-                        val dialog = new Dialog[BandMode] {
-                          initOwner(stage)
-                          title = "Select BandMode"
-                          headerText = s"Select BandMode for ${oldConfig.id}"
-                        }
-
-                        matrixPane.showConfigButton.value = false
-                        dialog.dialogPane().content = matrixPane.node
-                        dialog.dialogPane().buttonTypes = Seq(ButtonType.OK, ButtonType.Cancel)
-
-                        dialog.resultConverter = {
-                          case ButtonType.OK => selectedStore.selected.value
-                          case _ => null
-                        }
-
-                        val result = dialog.showAndWait()
-                        result match {
-                          case Some(bm: BandMode) =>
-                            nodeConfigManager.observableBuffer(index) = oldConfig.copy(bandMode = bm)
-                          case _ => // Cancelled
-                        }
-                      }
-                    }
-                  }
-                }
-                editable = false // Use modal instead of text field
-              },
-              new TableColumn[DebugConfig, java.lang.Boolean] {
-                text = "Startup Config"
-                cellValueFactory = { cd =>
-                  val prop = BooleanProperty(cd.value.showStartupConfig)
-                  prop.onChange { (_, _, newValue) =>
-                    val index = nodeConfigManager.observableBuffer.indexOf(cd.value)
-                    if (index >= 0) {
-                      val old = nodeConfigManager.observableBuffer(index)
-                      nodeConfigManager.observableBuffer(index) = old.copy(showStartupConfig = newValue)
-                    }
-                  }
-                  prop.delegate
-                }
-                cellFactory = CheckBoxTableCell.forTableColumn[DebugConfig](this)
-                editable = true
-              },
-              new TableColumn[DebugConfig, java.lang.Boolean] {
-                text = "Clear QSOs"
-                cellValueFactory = { cd =>
-                  val prop = BooleanProperty(cd.value.clearQsos)
-                  prop.onChange { (_, _, newValue) =>
-                    val index = nodeConfigManager.observableBuffer.indexOf(cd.value)
-                    if (index >= 0) {
-                      val old = nodeConfigManager.observableBuffer(index)
-                      nodeConfigManager.observableBuffer(index) = old.copy(clearQsos = newValue)
-                    }
-                  }
-                  prop.delegate
-                }
-                cellFactory = CheckBoxTableCell.forTableColumn[DebugConfig](this)
-                editable = true
-              },
-              new TableColumn[DebugConfig, DebugConfig] {
-                text = "Delete"
-                cellValueFactory = { cd => ObjectProperty(cd.value) }
-                cellFactory = (col: TableColumn[DebugConfig, DebugConfig]) => {
-                  new TableCell[DebugConfig, DebugConfig] {
-                    item.onChange { (_, _, debugConfig) =>
-                      graphic = if (debugConfig != null) {
-                        new Button("Delete") {
-                          onAction = _ => nodeConfigManager.observableBuffer -= debugConfig
-                        }
-                      } else null
-                    }
-                  }
-                }
-              }
-            )
-          }
+          center = new NodeConfigGridPane(
+            nodeConfigManager = nodeConfigManager,
+            injector = injector,
+            ownerWindow = stage
+          )
           bottom = new HBox {
             spacing = 10
             children = Seq(
@@ -242,5 +111,5 @@ object ManagerApp extends JFXApp3 with LazyLogging :
         }
       }
     }
-  
-}
+
+  }
