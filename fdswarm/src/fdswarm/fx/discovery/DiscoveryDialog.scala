a81d399ddf5125a30aec4ee4fb2a0b4f40aacfc5
dlieber <dick@u505.com>
2026-03-25 15:04:48 -0500

    new table builder works, styles not working.
    Signed-off-by: dlieber <dick@u505.com>

--- fdswarm/src/fdswarm/fx/discovery/DiscoveryDialog.scala
+++ fdswarm/src/fdswarm/fx/discovery/DiscoveryDialog.scala
@@ -1,79 +1,148 @@
-/*
- * Copyright (c) 2026. Dick Lieber, WA9NNN
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 package fdswarm.fx.discovery
 
 import com.typesafe.scalalogging.LazyLogging
-import fdswarm.fx.utils.GridTableBuilder
+import fdswarm.fx.utils.GridColumn
+import fdswarm.fx.utils.GridColumnAlignment
+import fdswarm.fx.utils.GridColumnWidth
+import fdswarm.fx.utils.GridRowBehavior
+import fdswarm.fx.utils.TypedGridTableBuilder
 import jakarta.inject.Inject
 import scalafx.Includes.*
 import scalafx.application.Platform
-import scalafx.geometry.Insets
-import scalafx.scene.control.*
-import scalafx.scene.layout.GridPane
+import scalafx.scene.Node
+import scalafx.scene.control.Button
+import scalafx.scene.control.ButtonType
+import scalafx.scene.control.Dialog
+import scalafx.scene.control.ScrollPane
+import scalafx.scene.layout.Region
 
 import scala.collection.mutable.ArrayBuffer
 
 class DiscoveryDialog @Inject() (contestDiscovery: ContestDiscovery)
-  extends Dialog with LazyLogging:
+  extends Dialog[ButtonType] with LazyLogging:
 
-  //  def show(window: Window): Unit =
-  val discovered = ArrayBuffer[NodeContestStation]()
-  contestDiscovery.discoverContest((ncs: NodeContestStation) =>
-    logger.info(s"Discovery UI added: $ncs")
-    discovered.append(ncs)
-    Platform.runLater {
-      populateGrid()
+  private type Ncs = NodeContestStation
+
+  private val discovered = ArrayBuffer.empty[Ncs]
+
+  private def textCol(
+                       header: String,
+                       sortable: Boolean = false,
+                       width: GridColumnWidth = GridColumnWidth.flexible(Region.USE_COMPUTED_SIZE),
+                       alignment: GridColumnAlignment = GridColumnAlignment.Left,
+                       cellStyleClasses: Ncs => Seq[String] = (_: Ncs) => Seq.empty[String]
+                     )(
+                       value: Ncs => String
+                     ): GridColumn[Ncs] =
+    GridColumn.text[Ncs](
+      header = header,
+      value = value,
+      cellStyleClasses = cellStyleClasses,
+      sortable = sortable,
+      alignment = alignment,
+      width = width
+    )
+
+  private def nodeCol(
+                       header: String,
+                       width: GridColumnWidth = GridColumnWidth.flexible(Region.USE_COMPUTED_SIZE),
+                       alignment: GridColumnAlignment = GridColumnAlignment.Left,
+                       cellStyleClasses: Ncs => Seq[String] = (_: Ncs) => Seq.empty[String],
+                       sortValue: Option[Ncs => String] = None
+                     )(
+                       value: Ncs => Node
+                     ): GridColumn[Ncs] =
+    GridColumn.node[Ncs](
+      header = header,
+      value = value,
+      cellStyleClasses = cellStyleClasses,
+      sortValue = sortValue,
+      alignment = alignment,
+      width = width
+    )
+
+  private val table = TypedGridTableBuilder(
+    GridRowBehavior[Ncs](
+      rowStyleClasses = ncs =>
+        if ncs.exchange.trim.isEmpty then Seq("row-warning") else Seq.empty,
+      onClick = Some(ncs =>
+        logger.info(s"Selected discovery row: $ncs")
+      ),
+      onDoubleClick = Some(ncs =>
+        logger.info(s"Double-clicked discovery row: $ncs")
+      )
+    ),
+    textCol(
+      header = "Host IP",
+      sortable = true,
+      width = GridColumnWidth.fixed(140)
+    )(_.nodeIdentity.hostIp),
+    textCol(
+      header = "Host Name",
+      sortable = true,
+      width = GridColumnWidth.flexible(180)
+    )(_.nodeIdentity.hostName),
+    textCol(
+      header = "Port",
+      sortable = true,
+      alignment = GridColumnAlignment.Right,
+      width = GridColumnWidth.fixed(80)
+    )(_.nodeIdentity.port.toString),
+    textCol(
+      header = "Contest",
+      sortable = true,
+      width = GridColumnWidth.flexible(120)
+    )(_.discoveryWire.contestConfig.contestType.toString),
+    textCol(
+      header = "Exchange",
+      sortable = true,
+      width = GridColumnWidth.flexible(120),
+      cellStyleClasses = ncs =>
+        if ncs.exchange.trim.isEmpty then Seq("cell-missing") else Seq("cell-ok")
+    )(_.exchange),
+    textCol(
+      header = "Our Call",
+      sortable = true,
+      width = GridColumnWidth.flexible(120)
+    )(_.discoveryWire.contestConfig.ourCallsign.toString),
+    textCol(
+      header = "Operator",
+      sortable = true,
+      width = GridColumnWidth.flexible(150)
+    )(_.discoveryWire.stationConfig.operator.toString),
+    nodeCol(
+      header = "Action",
+      sortValue = Some(_.nodeIdentity.hostIp),
+      alignment = GridColumnAlignment.Center,
+      width = GridColumnWidth.fixed(110)
+    ) { ncs =>
+      new Button("Connect"):
+        styleClass += "grid-inline-button"
+        onAction = _ =>
+          logger.info(s"Connect clicked for ${ncs.nodeIdentity.hostIp}:${ncs.nodeIdentity.port}")
     }
   )
-  logger.trace("Done waiting for responses from other nodes.")
+
+  private val scrollPane = new ScrollPane:
+    content = table.grid
+    fitToWidth = true
+    prefWidth = 1100
+    prefHeight = 500
+
   title = "Discovered Contest Stations"
   resizable = true
+  dialogPane().content = scrollPane
+  dialogPane().buttonTypes = Seq(ButtonType.OK)
 
-  var gridPane: GridPane = new GridPane {
-    hgap = 10
-    vgap = 10
-    padding = Insets(20, 100, 10, 10)
+  private def refreshGrid(): Unit =
+    table.setItems(discovered)
+
+  refreshGrid()
+
+  contestDiscovery.discoverContest { ncs =>
+    logger.info(s"Discovery UI added: $ncs")
+    Platform.runLater {
+      discovered += ncs
+      refreshGrid()
+    }
   }
-
-  def populateGrid(): Unit =
-    val gridTableBuilder: GridTableBuilder = GridTableBuilder()
-      .addHeaders("Host IP", "Host Name", "Port", "Contest", "Exchange", "Our Call", "Operator")
-    discovered.foreach { ncs =>
-      gridTableBuilder.addRow(
-        ncs.nodeIdentity.hostIp,
-        ncs.nodeIdentity.hostName,
-        ncs.nodeIdentity.port.toString,
-        ncs.discoveryWire.contestConfig.contestType.toString,
-        ncs.exchange,
-        ncs.discoveryWire.contestConfig.ourCallsign.toString,
-        ncs.discoveryWire.stationConfig.operator.toString
-      )
-    }
-    gridPane = gridTableBuilder.grid
-
-    val scrollPane: ScrollPane = new ScrollPane {
-      content = gridPane
-      prefWidth = 1000.0
-      prefHeight = 500.0
-    }
-
-    dialogPane().content = scrollPane
-    dialogPane().buttonTypes = Seq(ButtonType.OK)
-
-//      dialog.showAndWait()
---
+++ fdswarm/src/fdswarm/fx/utils/TypedGridTableBuilder.scala
@@ -0,0 +1,305 @@
+package fdswarm.fx.utils
+import scalafx.Includes.*
+import scalafx.beans.property.ObjectProperty
+import scalafx.geometry.Insets
+import scalafx.geometry.Pos
+import scalafx.scene.Node
+import scalafx.scene.control.Label
+import scalafx.scene.input.MouseEvent
+import scalafx.scene.layout.ColumnConstraints
+import scalafx.scene.layout.GridPane
+import scalafx.scene.layout.Priority
+import scalafx.scene.layout.Region
+import scalafx.scene.layout.StackPane
+
+sealed trait GridCellValue
+
+object GridCellValue:
+  final case class Text(value: String) extends GridCellValue
+  final case class NodeValue(node: Node) extends GridCellValue
+
+enum GridColumnAlignment:
+  case Left, Center, Right
+
+object GridColumnAlignment:
+  def styleClassOf(alignment: GridColumnAlignment): String =
+    alignment match
+      case GridColumnAlignment.Left   => "grid-align-left"
+      case GridColumnAlignment.Center => "grid-align-center"
+      case GridColumnAlignment.Right  => "grid-align-right"
+
+sealed trait GridColumnWidth
+
+object GridColumnWidth:
+  final case class Fixed(value: Double) extends GridColumnWidth
+  final case class Flexible(pref: Double) extends GridColumnWidth
+
+  def fixed(value: Double): GridColumnWidth = Fixed(value)
+  def flexible(pref: Double): GridColumnWidth = Flexible(pref)
+
+final case class GridRowBehavior[A](
+                                     rowStyleClasses: A => Seq[String] = (_: A) => Seq.empty[String],
+                                     onClick: Option[A => Unit] = None,
+                                     onDoubleClick: Option[A => Unit] = None
+                                   )
+
+object GridRowBehavior:
+  def apply[A](
+                rowStyleClasses: A => Seq[String],
+                onClick: A => Unit
+              ): GridRowBehavior[A] =
+    new GridRowBehavior[A](
+      rowStyleClasses = rowStyleClasses,
+      onClick = Some(onClick),
+      onDoubleClick = None
+    )
+
+  def empty[A]: GridRowBehavior[A] =
+    GridRowBehavior[A]()
+
+final case class GridColumn[A](
+                                header: String,
+                                render: A => GridCellValue,
+                                cellStyleClasses: A => Seq[String] = (_: A) => Seq.empty[String],
+                                sortValue: Option[A => String] = None,
+                                headerStyleClasses: Seq[String] = Seq.empty,
+                                alignment: GridColumnAlignment = GridColumnAlignment.Left,
+                                width: GridColumnWidth = GridColumnWidth.flexible(Region.USE_COMPUTED_SIZE)
+                              )
+
+object GridColumn:
+
+  def text[A](
+               header: String,
+               value: A => String,
+               cellStyleClasses: A => Seq[String] = (_: A) => Seq.empty[String],
+               sortable: Boolean = false,
+               sortValue: Option[A => String] = None,
+               headerStyleClasses: Seq[String] = Seq.empty,
+               alignment: GridColumnAlignment = GridColumnAlignment.Left,
+               width: GridColumnWidth = GridColumnWidth.flexible(Region.USE_COMPUTED_SIZE)
+             ): GridColumn[A] =
+    GridColumn(
+      header = header,
+      render = (a: A) => GridCellValue.Text(value(a)),
+      cellStyleClasses = cellStyleClasses,
+      sortValue =
+        if sortable then Some(sortValue.getOrElse(value))
+        else None,
+      headerStyleClasses = headerStyleClasses,
+      alignment = alignment,
+      width = width
+    )
+
+  def node[A](
+               header: String,
+               value: A => Node,
+               cellStyleClasses: A => Seq[String] = (_: A) => Seq.empty[String],
+               sortValue: Option[A => String] = None,
+               headerStyleClasses: Seq[String] = Seq.empty,
+               alignment: GridColumnAlignment = GridColumnAlignment.Left,
+               width: GridColumnWidth = GridColumnWidth.flexible(Region.USE_COMPUTED_SIZE)
+             ): GridColumn[A] =
+    GridColumn(
+      header = header,
+      render = (a: A) => GridCellValue.NodeValue(value(a)),
+      cellStyleClasses = cellStyleClasses,
+      sortValue = sortValue,
+      headerStyleClasses = headerStyleClasses,
+      alignment = alignment,
+      width = width
+    )
+
+final class GridHeaderLabel(text0: String = "") extends Label(text0):
+  styleClass += "grid-header-label"
+  maxWidth = Double.MaxValue
+
+object GridHeaderLabel:
+  def apply(text: String): GridHeaderLabel = new GridHeaderLabel(text)
+
+final class GridBodyLabel(text0: String = "") extends Label(text0):
+  styleClass += "grid-body-label"
+  maxWidth = Double.MaxValue
+
+object GridBodyLabel:
+  def apply(text: String): GridBodyLabel = new GridBodyLabel(text)
+
+final class GridTableCell private (
+                                    content0: Node,
+                                    styleClassNames: Seq[String]
+                                  ) extends StackPane:
+  styleClass += "grid-table-cell"
+  styleClass ++= styleClassNames
+  alignment = Pos.CenterLeft
+  padding = Insets(6, 8, 6, 8)
+  maxWidth = Double.MaxValue
+  children = Seq(content0)
+
+object GridTableCell:
+  def header(
+              content: Node,
+              extraStyleClasses: Seq[String] = Seq.empty
+            ): GridTableCell =
+    new GridTableCell(content, Seq("grid-header-cell") ++ extraStyleClasses)
+
+  def body(
+            content: Node,
+            extraStyleClasses: Seq[String] = Seq.empty
+          ): GridTableCell =
+    new GridTableCell(content, Seq("grid-body-cell") ++ extraStyleClasses)
+
+final case class GridSortState(
+                                columnIndex: Int,
+                                ascending: Boolean
+                              )
+
+final class TypedGridTableBuilder[A] private (
+                                               val columns: Seq[GridColumn[A]],
+                                               val rowBehavior: GridRowBehavior[A],
+                                               val grid: GridPane
+                                             ):
+
+  private var configuredColumns = 0
+  private var items: Vector[A] = Vector.empty
+
+  val sortState: ObjectProperty[Option[GridSortState]] =
+    ObjectProperty[Option[GridSortState]](this, "sortState", None)
+
+  private def ensureColumnConstraints(requiredColumns: Int): Unit =
+    if requiredColumns > configuredColumns then
+      for i <- configuredColumns until requiredColumns do
+        val c = columns(i)
+        val cc = new ColumnConstraints
+        c.width match
+          case GridColumnWidth.Fixed(value) =>
+            cc.hgrow = Priority.Never
+            cc.fillWidth = true
+            cc.minWidth = value
+            cc.prefWidth = value
+            cc.maxWidth = value
+          case GridColumnWidth.Flexible(pref) =>
+            cc.hgrow = Priority.Always
+            cc.fillWidth = true
+            cc.minWidth = Region.USE_COMPUTED_SIZE
+            cc.prefWidth = pref
+            cc.maxWidth = Double.MaxValue
+        grid.columnConstraints.add(cc)
+      configuredColumns = requiredColumns
+
+  private def sortIndicatorFor(columnIndex: Int): String =
+    sortState.value match
+      case Some(GridSortState(idx, asc)) if idx == columnIndex =>
+        if asc then " ▲" else " ▼"
+      case _ => ""
+
+  private def sortedItems: Vector[A] =
+    sortState.value match
+      case Some(GridSortState(columnIndex, ascending)) =>
+        columns(columnIndex).sortValue match
+          case Some(fn) =>
+            val sorted = items.sortBy(fn)
+            if ascending then sorted else sorted.reverse
+          case None =>
+            items
+      case None =>
+        items
+
+  private def toggleSort(columnIndex: Int): Unit =
+    columns(columnIndex).sortValue.foreach { _ =>
+      val next =
+        sortState.value match
+          case Some(GridSortState(idx, asc)) if idx == columnIndex =>
+            Some(GridSortState(columnIndex, !asc))
+          case _ =>
+            Some(GridSortState(columnIndex, true))
+      sortState.value = next
+      rebuild()
+    }
+
+  private def buildHeaderCell(col: GridColumn[A], idx: Int): GridTableCell =
+    val label = GridHeaderLabel(col.header + sortIndicatorFor(idx))
+    val cell = GridTableCell.header(
+      label,
+      col.headerStyleClasses ++
+        Seq(GridColumnAlignment.styleClassOf(col.alignment)) ++
+        (if col.sortValue.isDefined then Seq("grid-sortable-header-cell") else Seq.empty)
+    )
+
+    if col.sortValue.isDefined then
+      cell.onMouseClicked = _ => toggleSort(idx)
+
+    GridPane.setHgrow(cell, Priority.Always)
+    cell
+
+  private def buildBodyCell(
+                             item: A,
+                             rowIndex: Int,
+                             col: GridColumn[A]
+                           ): GridTableCell =
+    val node: Node =
+      col.render(item) match
+        case GridCellValue.Text(v)      => GridBodyLabel(v)
+        case GridCellValue.NodeValue(n) => n
+
+    val zebra =
+      if rowIndex % 2 == 0 then Seq("grid-row-even") else Seq("grid-row-odd")
+
+    val cell = GridTableCell.body(
+      node,
+      zebra ++
+        rowBehavior.rowStyleClasses(item) ++
+        col.cellStyleClasses(item) ++
+        Seq(GridColumnAlignment.styleClassOf(col.alignment))
+    )
+
+    rowBehavior.onClick.foreach { f =>
+      cell.onMouseClicked = e =>
+        if e.clickCount == 1 then f(item)
+        rowBehavior.onDoubleClick.foreach { g =>
+          if e.clickCount == 2 then g(item)
+        }
+    }
+
+    if rowBehavior.onClick.isEmpty then
+      rowBehavior.onDoubleClick.foreach { g =>
+        cell.onMouseClicked = e =>
+          if e.clickCount == 2 then g(item)
+      }
+
+    GridPane.setHgrow(cell, Priority.Always)
+    cell
+
+  def setItems(newItems: IterableOnce[A]): Unit =
+    items = newItems.iterator.toVector
+    rebuild()
+
+  def rebuild(): Unit =
+    grid.children.clear()
+    ensureColumnConstraints(columns.length)
+
+    columns.zipWithIndex.foreach { (c, i) =>
+      grid.add(buildHeaderCell(c, i), i, 0)
+    }
+
+    sortedItems.zipWithIndex.foreach { (item, row) =>
+      columns.zipWithIndex.foreach { (c, col) =>
+        grid.add(buildBodyCell(item, row, c), col, row + 1)
+      }
+    }
+
+object TypedGridTableBuilder:
+  def apply[A](
+                columns: GridColumn[A]*
+              ): TypedGridTableBuilder[A] =
+    apply[A](rowBehavior = GridRowBehavior.empty[A], columns = columns*)
+
+  def apply[A](
+                rowBehavior: GridRowBehavior[A],
+                columns: GridColumn[A]*
+              ): TypedGridTableBuilder[A] =
+    val g = new GridPane:
+      hgap = 0
+      vgap = 0
+      styleClass += "grid-table"
+
+    new TypedGridTableBuilder[A](columns.toSeq, rowBehavior, g)
