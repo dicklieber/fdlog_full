3f24b035b6b3bab8eabd18afe9f4ea3bc45a9f91
dlieber <dick@u505.com>
2026-03-25 12:56:02 -0500

    styles not working
    Signed-off-by: dlieber <dick@u505.com>

--- fdswarm/src/fdswarm/fx/utils/GridTableBuilder.scala
+++ fdswarm/src/fdswarm/fx/utils/GridTableBuilder.scala
@@ -29,10 +29,10 @@ class GridTableBuilder(val grid: GridPane):
   private var row = 0
 
   def addHeaders(headers: String*): GridTableBuilder =
-    headers.zipWithIndex.foreach((h, c) =>
-      val cell = GridTableCell.header(GridHeaderLabel(h))
+    headers.zipWithIndex.foreach((headerText, column) =>
+      val cell = GridTableCell.header(GridHeaderLabel(headerText))
       GridPane.setHgrow(cell, Priority.Always)
-      grid.add(cell, c, row)
+      grid.add(cell, column, row)
     )
     row += 1
     this
@@ -49,5 +49,6 @@ class GridTableBuilder(val grid: GridPane):
 object GridTableBuilder:
   def apply(): GridTableBuilder =
     val g = new GridPane()
+    g.gridLinesVisible = true
     g.styleClass += "grid-table"
     new GridTableBuilder(g)
