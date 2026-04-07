cd758153cf0c67a607022d58acb3a0a2acc0b55f
dlieber <dick@u505.com>
2026-04-07 09:23:19 -0500

    split NodeDataField and FdHours from SwarmData to separate file.

---
+++ fdswarm/src/fdswarm/replication/status/NodeDataField.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2026. Dick Lieber, WA9NNN
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package fdswarm.replication.status
+
+enum NodeDataField:
+  case HostIp
+  case Port
+  case HostName
+  case InstanceId
+  case Received
+  case IsLocal
+  case QsoCount
+  case StatusId
+  case Operator
+  case Band
+  case Mode
+  case BandModeStamp
+  case ContestType
+  case ContestCallsign
+  case ContestTransmitters
+  case ContestClass
+  case ContestSection
+  case ContestStamp
+  case FdHoursField(fdHour: FdHours)
+
+  def label: String = this match
+    case HostIp => "hostIp"
+    case Port => "port"
+    case HostName => "hostName"
+    case InstanceId => "instanceId"
+    case Received => "received"
+    case IsLocal => "isLocal"
+    case QsoCount => "qsoCount"
+    case StatusId => "statusId"
+    case Operator => "operator"
+    case Band => "band"
+    case Mode => "mode"
+    case BandModeStamp => "bandModeStamp"
+    case ContestType => "contestType"
+    case ContestCallsign => "contestCallsign"
+    case ContestTransmitters => "contestTransmitters"
+    case ContestClass => "contestClass"
+    case ContestSection => "contestSection"
+    case ContestStamp => "contestStamp"
+    case FdHoursField(fdHour) => fdHour.label
+
+object NodeDataField:
+  val staticFields: Seq[NodeDataField] = Seq(
+    NodeDataField.HostIp,
+    NodeDataField.Port,
+    NodeDataField.HostName,
+    NodeDataField.InstanceId,
+    NodeDataField.Received,
+    NodeDataField.IsLocal,
+    NodeDataField.QsoCount,
+    NodeDataField.StatusId,
+    NodeDataField.Operator,
+    NodeDataField.Band,
+    NodeDataField.Mode,
+    NodeDataField.BandModeStamp,
+    NodeDataField.ContestType,
+    NodeDataField.ContestCallsign,
+    NodeDataField.ContestTransmitters,
+    NodeDataField.ContestClass,
+    NodeDataField.ContestSection,
+    NodeDataField.ContestStamp
+  )
